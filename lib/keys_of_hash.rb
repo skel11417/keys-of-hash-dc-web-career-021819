1f10aed30a2be8e1c9f61b79aba91d05dd5116a4
Sean Kelly <shonkelli@gmail.com>
2019-02-06 17:54:56 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,10 +3,8 @@ class Hash
   def keys_of(arg1, **args)
     output = []
     self.each do |k,v|
-      args.each do |arg|
-        if v == arg 
-          output << key
-        end
+      if v = arg1
+        output << k
       end
     end
     output
