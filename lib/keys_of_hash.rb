ac704264f9eafb03d41d1bbbfe1542d6ecaea06b
Sean Kelly <shonkelli@gmail.com>
2019-02-06 18:04:07 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,9 +3,7 @@ class Hash
   def keys_of(arg1, *args)
     output = []
     self.each do |k,v|
-      binding.pry
       if v == arg1 || args.include?(v)
-        binding.pry
         output << k
         
       end
